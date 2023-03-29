ac35539b53fc2a83e99567e3ad5b702bbb2be4ab
aadhil-aslam <m.aadhilaslam@gmail.com>
2023-03-29 10:02:24 +0530

    first commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,15 +5,16 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
-
+import { NgxOtpInputModule } from 'ngx-otp-input';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    NgxOtpInputModule,
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
 })
 export class HomePageModule {}
